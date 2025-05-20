3c9dd3a382ed5fccf0cc9f6e6cf31982b4953098
Lirkul <125795162+Lirkul@users.noreply.github.com>
2025-05-20 16:17:31 +0700

    Update container_script.sh

--- Lab2/container_script.sh
+++ Lab2/container_script.sh
@@ -7,40 +7,36 @@ COUNTER=1
 
 mkdir -p "$SHARED_DIR"
 
-
-create_file() {
+create_and_write_file() {
     (
-        flock -x 200
+        flock -x 200  
+
 
         for i in $(seq -w 1 999); do
             if [ ! -f "$SHARED_DIR/$i" ]; then
-                echo "$i"
-                return
+
+                echo "Container: $CONTAINER_ID, File number: $COUNTER" > "$SHARED_DIR/$i"
+                echo "$i"  
+                return 0
             fi
         done
-        echo ""
+        return 1  
     ) 200>"$LOCK_FILE"
 }
 
 while true; do
-
-    FILE_NAME=$(create_file)
-    
-    if [ -n "$FILE_NAME" ]; then
-
-        echo "Container: $CONTAINER_ID, File number: $COUNTER" > "$SHARED_DIR/$FILE_NAME"
+    if FILE_NAME=$(create_and_write_file); then
         COUNTER=$((COUNTER + 1))
-
-
-        sleep 1
+        sleep 1  
 
 
         (
             flock -x 200
             rm -f "$SHARED_DIR/$FILE_NAME"
         ) 200>"$LOCK_FILE"
+    else
+        echo "No available file names (001-999 are occupied)"
     fi
 
-
-    sleep 1
+    sleep 1  
 done
